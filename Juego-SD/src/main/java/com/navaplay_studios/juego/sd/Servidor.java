e4c7be814e0bb81a9ee66e35aa43847ea7d51dc4
Fernando García <148652897+FerGaram@users.noreply.github.com>
2025-05-26 08:31:18 -0600

    Archivos vacíos creados
    Listos para empezar a programar la lógica de Cliente y Servidor

---
+++ Juego-SD/src/main/java/com/navaplay_studios/juego/sd/Servidor.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.navaplay_studios.juego.sd;
+
+/**
+ *
+ * @author ferga
+ */
+public class Servidor {
+    
+}
